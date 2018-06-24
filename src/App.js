ab42d09f5de1291d29f7e104cee68ef494b94932
Sebastian <svnineseven@gmail.com>
2018-06-24 14:00:11 -0400

    styled results section and covered edge cases where api does not respond

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import CityButtons from './Buttons.jsx'
 import Loading from './Loading.jsx'
+import Results from './Results.jsx'
+import testData from './testData.js'
 
 class App extends Component {
 
@@ -10,12 +12,19 @@ class App extends Component {
   }
 
   getWeatherData = async (city) => {
-    this.setState({isLoading: true, weatherData: null}) // clear state every time so loading state can render
-    let weatherData = await (await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},Canada&APPID=f2fa254ec5f9ad4ce8515f36707b683e`)).json()
-    setTimeout( () => {
-      this.setState({weatherData: weatherData, isLoading: false})
-    }, 2000)
+    this.setState({isLoading: true, weatherData: null}) // clears state every time so loading state can render
 
+    try {
+      var weatherData = await (await fetch(`http://api.openweathermap.org/1data/2.5/weather?q=${city},Canada&units=metric&APPID=7d6f7bb8698882389fd14b203d5bb8aa`)).json()
+    } catch (error) {
+      console.log(error)
+      var weatherData = null
+    }
+
+    setTimeout( () => {
+      //timeout to simulate loading
+      this.setState({weatherData: weatherData, isLoading: false, visibility: ""})
+    }, 1500)
   }
 
   render() {
@@ -24,39 +33,57 @@ class App extends Component {
     let isLoading = this.state.isLoading
 
     if (thereIsWeatherData) {
-
-      // Results Page
       return(
+
+        // Results Page
         <div className="masterContainer">
+
           <h1>Discover the weather in each of Canada's major cities.</h1>
 
           <CityButtons getWeatherData={this.getWeatherData}/>
 
           <div className="results">
-            Temperature: {this.state.weatherData.main.temp}
+            <Results weatherData={this.state.weatherData}/>
           </div>
+
         </div>
+
       )
-
     } else if (isLoading) {
-
-      // Loading State
       return(
+
+        // Loading State
         <div className="loading">
           <Loading />
         </div>
+
       )
-
-    } else {
-
-      // Landing Page
+    } else if(!thereIsWeatherData) {
       return(
+
+        <div className="masterContainer">
+
+          <h1>Discover the weather in each of Canada's major cities.</h1>
+
+          <CityButtons getWeatherData={this.getWeatherData}/>
+
+          <div className="results">
+            <h4>The Weather API is unresponsive. Please try again later.</h4>
+          </div>
+
+        </div>
+
+      )
+    } else {
+      return(
+
+        // Landing Page
         <div className="masterContainer">
           <h1>Discover the weather in each of Canada's major cities.</h1>
           <CityButtons getWeatherData={this.getWeatherData}/>
         </div>
-      )
 
+      )
     }
   }
 
---
+++ src/Results.jsx
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+
+class Results extends Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+
+    return(
+      <div>
+
+        <img src={`http://openweathermap.org/img/w/${this.props.weatherData.weather[0].icon}.png`}/>
+        <img src={`http://openweathermap.org/img/w/${this.props.weatherData.weather[0].icon}.png`}/>
+        <img src={`http://openweathermap.org/img/w/${this.props.weatherData.weather[0].icon}.png`}/>
+
+        <div>
+          <p>Minimum Temperature: {this.props.weatherData.main.temp_min}</p>
+          <p>Temperature: {this.props.weatherData.main.temp}</p>
+          <p>Maximum Temperature: {this.props.weatherData.main.temp_max}</p>
+          <p>Humidity: {this.props.weatherData.main.humidity}</p>
+        </div>
+
+      </div>
+
+    )
+  }
+
+}
+
+export default Results;
